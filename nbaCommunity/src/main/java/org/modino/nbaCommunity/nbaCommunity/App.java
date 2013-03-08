35d6efcff7e3450d543fded86ae0776de8ef6bc1
David Modino <modinodavid@gmail.com>
2013-03-08 15:24:12 +0100

    Create project nbaCommunity

---
+++ nbaCommunity/src/main/java/org/modino/nbaCommunity/nbaCommunity/App.java
@@ -0,0 +1,13 @@
+package org.modino.nbaCommunity.nbaCommunity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ nbaCommunity/src/main/java/org/modino/nbaCommunity/nbaCommunity/Usuario.java
@@ -0,0 +1,5 @@
+package org.modino.nbaCommunity.nbaCommunity;
+
+public class Usuario {
+
+}
