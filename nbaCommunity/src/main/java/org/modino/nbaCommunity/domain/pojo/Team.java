c839e6b71080cde74ed12fe38f6a861119fadbf7
David Modino <modinodavid@gmail.com>
2013-03-14 00:22:43 +0100

    Added hibernate annotations, implementation of the hashCode, equals and toString methods

--- nbaCommunity/src/main/java/org/modino/nbaCommunity/domain/pojo/Team.java
+++ nbaCommunity/src/main/java/org/modino/nbaCommunity/domain/pojo/Team.java
@@ -1,22 +1,61 @@
 package org.modino.nbaCommunity.domain.pojo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.modino.nbaCommunity.dao.hibernate.HibernateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Named("team")
 @SessionScoped
+@Entity
+@Table(name="users")
+@DynamicInsert(false)
+//Solo las columnas modificadas serán actualizadas en tiempo de ejecucion
+@DynamicUpdate(true)
+
 public class Team extends NBATeam implements Serializable {
 
 	private static final long serialVersionUID = 7120245471043503643L;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="salary")
 	private double currentSalary;
+	
 	private int maxPlayers; //12 jugadores
+	
+	@Transient
 	private List<Player> players;
 	
 	public Team() {
 		super();
+		this.players = new ArrayList<Player>();
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public double getCurrentSalary() {
@@ -42,6 +81,22 @@ public class Team extends NBATeam implements Serializable {
 	public void setPlayers(List<Player> players) {
 		this.players = players;
 	}
+
+	public void addPlayer(Player player){
+		if(this.players == null){
+			this.players = new ArrayList<Player>();
+		}
+		this.players.add(player);
+		logger.info("One player has added to your team");
+	}
 	
-	
+	public void removePlayer(Player player){
+		if(this.players != null){
+			this.players.remove(player);
+			logger.info("The player has removed from your team");
+		}
+		else{
+			logger.info("Unable to remove the player from your team");
+		}
+	}
 }
--- nbaCommunity/src/main/java/org/modino/nbaCommunity/domain/pojo/User.java
+++ nbaCommunity/src/main/java/org/modino/nbaCommunity/domain/pojo/User.java
@@ -5,14 +5,36 @@ import java.util.Date;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Named
 @SessionScoped
+@Entity
+@Table(name="users")
+@DynamicInsert(false)
+//Solo las columnas modificadas serán actualizadas en tiempo de ejecucion
+@DynamicUpdate(true)
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 3323664210787119963L;
+	
+	public static enum Type {User, Admin};
+	public static enum Gender {Male, Female};
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
 	//tipo User o Administrator
-	private String type;
+	private Type type;
 	
 	//Campos obligatorios
 	private String username;
@@ -21,17 +43,33 @@ public class User implements Serializable{
 	private String email;
 	
 	//Campos opcionales
-	private String nick;
+	private String avatar;
 	private String firstName;
 	private String lastName;
 	private Date birthday;
-	private String sex;
-	private String province;
-	private int postalCode;
-	private String pais;
+	private Gender gender;
+	
+	// @Embedded -> Define los campos del componente como columnas en la tabla users de BD 
+	@Embedded
+	private Address address; 
+	
+	// @Transient -> Se relaciona con este objeto pero no se añaden sus campos como columnas de la tabla  
+	@Transient
+	private League league;
+	
+	@Transient
+	private Team team;
 	
 	public User() {
 		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getUsername() {
@@ -66,14 +104,6 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
-	public String getNick() {
-		return nick;
-	}
-
-	public void setNick(String nick) {
-		this.nick = nick;
-	}
-
 	public String getFirstName() {
 		return firstName;
 	}
@@ -98,46 +128,163 @@ public class User implements Serializable{
 		this.birthday = birthday;
 	}
 
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public String getProvince() {
-		return province;
-	}
-
-	public void setProvince(String province) {
-		this.province = province;
-	}
-
-	public int getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(int postalCode) {
-		this.postalCode = postalCode;
-	}
-
-	public String getPais() {
-		return pais;
-	}
-
-	public void setPais(String pais) {
-		this.pais = pais;
-	}
-
-	public String getType() {
+	public Type getType() {
 		return type;
 	}
 
-	public void setType(String type) {
+	public void setType(Type type) {
 		this.type = type;
 	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public League getLeague() {
+		return league;
+	}
+
+	public void setLeague(League league) {
+		this.league = league;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((avatar == null) ? 0 : avatar.hashCode());
+		result = prime * result
+				+ ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + id;
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((league == null) ? 0 : league.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((repassword == null) ? 0 : repassword.hashCode());
+		result = prime * result + ((team == null) ? 0 : team.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (avatar == null) {
+			if (other.avatar != null)
+				return false;
+		} else if (!avatar.equals(other.avatar))
+			return false;
+		if (birthday == null) {
+			if (other.birthday != null)
+				return false;
+		} else if (!birthday.equals(other.birthday))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender != other.gender)
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (league == null) {
+			if (other.league != null)
+				return false;
+		} else if (!league.equals(other.league))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (repassword == null) {
+			if (other.repassword != null)
+				return false;
+		} else if (!repassword.equals(other.repassword))
+			return false;
+		if (team == null) {
+			if (other.team != null)
+				return false;
+		} else if (!team.equals(other.team))
+			return false;
+		if (type != other.type)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
 	
-	
-	
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", email=" + email + ", avatar="
+				+ avatar + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", birthday=" + birthday + ", gender=" + gender
+				+ ", league=" + league + ", team=" + team + "]";
+	}
+
+
+		
 }
