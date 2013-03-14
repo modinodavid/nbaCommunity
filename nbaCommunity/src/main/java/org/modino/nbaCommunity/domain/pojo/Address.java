802f05c56d7f8b28c3389a917600005eaf1186b4
David Modino <modinodavid@gmail.com>
2013-03-14 10:25:44 +0100

    Added Address bean

---
+++ nbaCommunity/src/main/java/org/modino/nbaCommunity/domain/pojo/Address.java
@@ -0,0 +1,105 @@
+package org.modino.nbaCommunity.domain.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address implements Serializable{
+
+	private static final long serialVersionUID = -2276842786703078539L;
+	private String street;
+	private String postalCode;
+	private String province;
+	private String country;
+	
+	public Address() {
+		super();
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public String getProvince() {
+		return province;
+	}
+	
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result
+				+ ((postalCode == null) ? 0 : postalCode.hashCode());
+		result = prime * result
+				+ ((province == null) ? 0 : province.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (postalCode == null) {
+			if (other.postalCode != null)
+				return false;
+		} else if (!postalCode.equals(other.postalCode))
+			return false;
+		if (province == null) {
+			if (other.province != null)
+				return false;
+		} else if (!province.equals(other.province))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", postalCode=" + postalCode
+				+ ", province=" + province + ", country=" + country + "]";
+	}
+
+	
+	
+}
